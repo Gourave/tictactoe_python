7e829b63b9a13fb1deb8285b70272b06473a7144
Goverm <gouravev@gmail.com>
2015-08-08 23:13:58 -0400

    Added magic square list to check for wins

--- __main__.py
+++ __main__.py
@@ -3,9 +3,6 @@ def print_board(board):
     for row in board:
         print (" ").join(row)
 
-# Checks to see if there is a winner yet
-def check_winner():
-    return False
 
 if __name__ == "__main__":
     # Setup the game board
@@ -13,14 +10,25 @@ if __name__ == "__main__":
     for i in range(0, 3):
         board.append(["- | - | -"])
 
+    # Magic square to hold the values of each tile
+    # Used to determine a winner
+    magic_square = [[8, 1, 6], [3, 5, 7], [4, 9, 2]]
+
     # Prompt the user to see whether he/she would like to play the computer or not
     print "Welcome to TicTacToe"
-    game_choice = raw_input("Would you like to play with another player, or the computer? ")
+    game_choice = raw_input("Would you like to play with another 'player', or the 'computer?' ")
 
     # If the input was of incorrect format, ask the user again until he/she gets it right
     while game_choice != "player" and game_choice != "computer":
         print "Please enter either 'player' or 'computer'"
         game_choice = raw_input("Would you like to play with another player, or the computer? ")
+
+    if game_choice == "player":
+        print "Playing against another player"
+        # Put code here for multiplayer game
+    else:
+        print "Playing against the computer"
+        # Put code here for Ai game
 
     # Prints the game board out
     print_board(board)
