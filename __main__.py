13d866e006ba70a05c5fc63bd71fee710b6a596d
Goverm <gouravev@gmail.com>
2015-08-10 21:53:00 -0400

    PvP code updated to allow user to pick place to place his/her piece

--- __main__.py
+++ __main__.py
@@ -1,13 +1,28 @@
 # Prints out the game board in a way the user can see it nicely
 def print_board(board):
     for row in board:
-        print (" ").join(row)
+        print " ".join(row)
+
+# Check to see whether there is a winner or not
+def winner(board):
+    return False
+
+# Prompt the user to make a move and take up the spot that was specified by
+# the players piece
+def make_move(board, player):
+    row = -1
+    col = -1
+    while not (-1 < row < 3):
+        row = int(raw_input("Row: "))
+    while not (-1 < col < 3):
+        col = int(raw_input("Col: "))
+    board[row][col] = player
 
 if __name__ == "__main__":
     # Setup the game board
     board = []
     for i in range(0, 3):
-        board.append(["- | - | -"])
+        board.append(["-",  "-",  "-"])
 
     # Magic square to hold the values of each tile
     # Used to determine a winner
@@ -23,13 +38,31 @@ if __name__ == "__main__":
         game_choice = raw_input("Would you like to play with another player, or the computer? ")
 
     if game_choice == "player":
-        print "Playing against another player"
-        # Put code here for multiplayer game
-        # Create class for the multiplayer game
+        player_one = "X"
+        player_two = "O"
+        player_one_turn = True
+
+        print "Player 1: " + player_one
+        print "Player 2: " + player_two
+
+        print_board(board)
+
+        while not winner(board):
+            if player_one_turn:
+                print "Player 1's turn"
+                make_move(board, player_one)
+                player_one_turn = False
+            else:
+                print "Player 2's turn"
+                make_move(board, player_two)
+                player_one_turn = True
+
+            print_board(board)
+
     else:
         print "Playing against the computer"
         # Put code here for Ai game
         # Create class for Ai game
 
     # Prints the game board out
-    print_board(board)
+
