396891bde49dd3bc5ae5937d856c65ea644061d2
Goverm <gouravev@gmail.com>
2015-09-28 10:32:08 -0400

    Made AI defense better, i hope...

--- __main__.py
+++ __main__.py
@@ -134,7 +134,10 @@ def ai_make_move(board, computer, human, board_checked):
         # TODO: Add defensive moves
         for row_taken in range(0, len(board)):
             if human in board[row_taken] and computer not in board[row_taken]:
-                row = row_taken
+                if row_taken == 2:
+                    row = 0
+                else:
+                    row = row_taken + 1
                 break
             elif computer in board[row_taken]:
                 if row_taken == 2:
@@ -147,8 +150,19 @@ def ai_make_move(board, computer, human, board_checked):
         transpose = zip(*board)
         for col_taken in range(0, len(transpose)):
             if human in transpose[col_taken] and computer not in transpose[col_taken]:
-                if board[row][col_taken + 1] != human:
-                    col = col_taken + 1
+                block = check_num_human_in_row(board, human, computer)
+                if board[row][col_taken] != human and block:
+                    col = col_taken
+                #TODO: Fix out of bounds error with col_taken + 1
+                elif board[row][col_taken + 1] != human:
+                    if row == 0:
+                        row = 2
+                    else:
+                        row -= 1
+                    if col_taken == 2:
+                        col = 0
+                    else:
+                        col = col_taken + 1
                 else:
                     if col_taken == 0:
                         col = 2
@@ -170,6 +184,21 @@ def ai_make_move(board, computer, human, board_checked):
         ai_make_move(board, computer, human, True)
     else:
         board[row][col] = computer
+
+
+# Check for the number of humans in a certain row
+def check_num_human_in_row(board, human, computer):
+    num_human = 0
+    for row in range(0, len(board)):
+        num_human = 0
+        for col in range(0, len(board)):
+            if human == board[row][col]:
+                num_human += 1
+                if num_human >= 2:
+                    return True
+            elif computer == board[row][col]:
+                num_human = 0
+    return False
 
 
 def main():
